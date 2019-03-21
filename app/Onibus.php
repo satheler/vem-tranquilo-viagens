eec76e0fbcc3ef9512dadf7de5c58b9f5b01d09d
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2019-03-21 12:15:01 -0300

    Model - Changed: Onibus

--- app/Onibus.php
+++ app/Onibus.php
@@ -2,15 +2,32 @@
 
 namespace App;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 class Onibus extends Model
 {
     protected $table = 'onibus';
 
-    public function inherit() {
-        return $this->morphTo();
+    public function getAll() {
+        return $this::all();
     }
 
+    public function add(bool $disponivel, bool $acessibilidade, float $custoManutencao, string $chassi, string $placa) {
+        $this->disponivel = $disponivel;
+        $this->acessibilidade = $acessibilidade;
+        $this->custoManutencao = $custoManutencao;
 
+        if(strlen($chassi) != 17) {
+            throw new Exception("Chassi: Quantidade de caracteres inválidos");
+        }
+        $this->chassi = $chassi;
+
+        if(strlen($placa) != 7) {
+            throw new Exception("Placa: Quantidade de caracteres inválidos");
+        }
+        $this->placa = $placa;
+
+        $this->save();
+    }
 }
