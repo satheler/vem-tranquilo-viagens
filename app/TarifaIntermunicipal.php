67d3e276a62020444bafe89cfc795f3efe500225
Sabrina <sabrinacarlew@gmail.com>
2019-03-28 22:32:53 -0300

    Model - Changed: Tarifa

--- app/TarifaIntermunicipal.php
+++ app/TarifaIntermunicipal.php
@@ -4,7 +4,29 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Validator;
+use Exception;
+
 class TarifaIntermunicipal extends Model
 {
-    //
+    protected $table= 'tarifa_intermunicipal';
+
+    public function description()
+    {
+        return $this->morphOne('App\Tarifa', 'description');
+    }
+
+    public function getAll()
+    {
+        return $this->all();
+    }
+
+    public function add(array $input)
+    {
+    $tarifa = new Tarifa();
+    $data = $tarifa->add($input);
+
+    $this->save();
+    $this->description()->save($data);
+    }
 }
--- app/TarifaLocal.php
+++ app/TarifaLocal.php
@@ -4,7 +4,45 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Validator;
+use Exception;
+
 class TarifaLocal extends Model
 {
-    //
+    protected $table= 'tarifa_local';
+
+    public function description()
+    {
+        return $this->morphOne('App\Tarifa', 'description');
+    }
+
+    public function getAll()
+    {
+        return $this->all();
+    }
+
+    public function add(array $input)
+    {
+
+        $validator = Validator::make($input, [
+            'cidade' => 'exists:cidade,cidade', //Verifica se a cidade existe na tabela cidade e coluna cidade.
+            'licitacao' => 'required|unique:tarifa',
+            'valorEspecial' => 'required|integer'
+
+        ]);
+
+        if ($validator->fails()) {
+            throw new Exception($validator->messages());
+        }
+
+        $this->cidade = $input['cidade'];
+        $this->licitacao = $input['licitacao'];
+        $this->valorEspecial = $input['valor_especial'];
+
+        $tarifa = new Tarifa();
+        $data = $tarifa->add($input);
+
+        $this->save();
+        $this->description()->save($data);
+    }
 }
