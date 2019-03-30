cf8f61a38ca4595b75dc3865396660f04c7af349
Sabrina <sabrinacarlew@gmail.com>
2019-03-29 21:06:41 -0300

    Database/Model - TrajetoIntermunicipal and Local

---
+++ app/TrajetoIntermunicipal.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Exception;
+
+class TrajetoIntermunicipal extends Model
+{
+
+}
---
+++ app/TrajetoLocal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Exception;
+use Validator;
+
+class TrajetoLocal extends Model
+{
+    
+}
