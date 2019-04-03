5f64f4a918fdff78f93aaf58eefc028c65a19363
Rodrigo Oliveira <roliveira.loki@gmail.com>
2019-04-03 12:13:19 -0300

    Controller - Changes: Passageiro
    Metodos parcialmente criados

---
+++ app/Http/Controllers/PassageiroController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PassageiroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $passageiro = new Passageiro();
+        $listaDePassageiros = $passageiro->getAll();
+        return view('passageiro', compact('listaDePassageiros'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('cadastroPassageiro');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+
+            $passageiro = new Passageiro();
+            $passageiro->add($request->input());
+            return response(["status" => "Tipo de passageiro cadastrado com sucesso"], 201);
+
+        } catch (Exception $e) {
+            return response($e->getMessage(), 400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $passageiro = new Passageiro();
+        $passageiro = $passageiro->get($id);
+        return response($passageiro->toJson(), 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $passageiro = new Passageiro();
+        $listaDePassageiros = $passageiro->getAll();
+        $passageiroEditado = $listaDePassageiros[$id];
+        return "Formulario de edição para o".$passageiroEditado->toJson();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+
+            $passageiroEditado = new Passageiro();
+            $passageiroEditado->edit($id);
+            return response(["status" => "Tipo de passageiro atualizado com sucesso"], 202);
+
+        } catch (Exception $e) {
+            return response($e->getMessage(), 400);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return response(['error' => 'Função não permitida.'], 501);
+    }
+}
