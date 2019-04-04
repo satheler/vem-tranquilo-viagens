bb6a8cf3bbaf8c9d2f142fb10cad727b57996460
Rodrigo Oliveira <roliveira.loki@gmail.com>
2019-04-04 09:45:58 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,22 @@
  */
 
 Route::get('/', function () {
-  return redirect('admin'); 
+    return redirect('home');
 });
 
-Route::get('/admin', function(){
-    return view('admin-index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::group(['middleware' => 'auth'], function () {
+Route::resource('user', 'UserController', ['except' => ['show']]);
+Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+// });
+
+Route::prefix('onibus')->group(function () {
+    Route::resource('urbano', 'OnibusUrbanoController');
+    Route::resource('intermunicipal', 'OnibusIntermunicipalController');
 });
 
-Route::prefix('admin')->group(function () {
-    Route::prefix('onibus')->group(function () {
-        Route::resource('urbano', 'OnibusUrbanoController');
-        Route::resource('intermunicipal', 'OnibusIntermunicipalController');
-    });
-});
