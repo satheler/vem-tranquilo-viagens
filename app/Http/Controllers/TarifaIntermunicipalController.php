392f2aa64ad2a3eda25c1e9aaa5ee9b2a94b5fe4
Rodrigo Oliveira <roliveira.loki@gmail.com>
2019-04-04 15:36:24 -0300

    Controller - update:Tarifa

--- app/Http/Controllers/TarifaIntermunicipalController.php
+++ app/Http/Controllers/TarifaIntermunicipalController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TarifaIntermunicipal;
 
 class TarifaIntermunicipalController extends Controller
 {
--- app/Http/Controllers/TarifaLocalController.php
+++ app/Http/Controllers/TarifaLocalController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TarifaLocal;
 
 class TarifaLocalController extends Controller
 {
