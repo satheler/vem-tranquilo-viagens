a0921f5132f2cda0370386344682ef4d3b82f95e
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2019-04-04 08:57:15 -0300

    Frontend - Added: Argon Dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,19 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('home');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::group(['middleware' => 'auth'], function () {
+Route::resource('user', 'UserController', ['except' => ['show']]);
+Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+// });
