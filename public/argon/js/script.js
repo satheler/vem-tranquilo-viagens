a7b6094ba60c6c7b6a4c11d4631121371200a5a3
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2019-04-08 20:51:26 -0300

    Frontend - Added: Pre-loader

--- public/argon/js/script.js
+++ public/argon/js/script.js
@@ -1,9 +1,12 @@
-// $(function () {
-    if($.fn.dataTable) {
-        $.extend(true, $.fn.dataTable.defaults, {
-            language: {
-                url: '/argon/vendor/datatables/dist/json/Portuguese-Brasil.json'
-            }
-        })
-    }
-// });
+if($.fn.dataTable) {
+    $.extend(true, $.fn.dataTable.defaults, {
+        language: {
+            url: '/argon/vendor/datatables/dist/json/Portuguese-Brasil.json'
+        }
+    })
+}
+
+$(window).on('load', function () {
+    setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 250);
+})
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,21 @@
 
     </head>
     <body class="{{ $class ?? '' }}">
+        <div class="page-loader-wrapper">
+            <div class="loader">
+                <div class="preloader">
+                    <div class="spinner-layer pl-purple">
+                        <div class="circle-clipper left">
+                            <div class="circle"></div>
+                        </div>
+                        <div class="circle-clipper right">
+                            <div class="circle"></div>
+                        </div>
+                    </div>
+                </div>
+                <p>Carregando... Aguarde...</p>
+            </div>
+        </div>
         {{-- @auth() --}}
             {{-- <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
