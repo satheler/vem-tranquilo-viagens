0c5c48b7fe334ae201dde99903924c1186512156
Sabrina <sabrinacarlew@gmail.com>
2019-04-11 13:53:59 -0300

    Model - Changed: AlocacaoUrbano

--- database/migrations/2019_04_11_123718_create_alocacao_intermunicipal_table.php
+++ database/migrations/2019_04_11_123718_create_alocacao_intermunicipal_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAlocacaosTable extends Migration
+class CreateAlocacaoIntermunicipalTable extends Migration
 {
     private $databaseName = 'alocacao_intermunicipal';
     /**
