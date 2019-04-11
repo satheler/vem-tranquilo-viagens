baf2cef5e6abbbde02ddcfe0e93a7596de6aa5dd
Michael Martins <martins.m09@hotmail.com>
2019-04-11 18:21:14 -0300

    Frontend - Changed: Tarifa Urbana

--- resources/views/tarifa/urbano/index.blade.php
+++ resources/views/tarifa/urbano/index.blade.php
@@ -26,50 +26,20 @@
             <table id="datatable-basic" class="table align-items-center table-flush dataTable">
                 <thead class="thead-light">
                     <tr>
-                        <th scope="col"></th>
-                        <th scope="col">{{ __('Placa') }}</th>
-                        <th scope="col">{{ __('Chassi') }}</th>
-                        <th scope="col">{{ __('Lotação') }}</th>
-                        <th scope="col">{{ __('Ar condicionado') }}</th>
-                        <th scope="col">{{ __('Acessibilidade') }}</th>
-                        <th scope="col">{{ __('Disponibilidade') }}</th>
+                        <th scope="col">{{ __('Cidade') }}</th>
+                        <th scope="col">{{ __('Licitação') }}</th>
+                        <th scope="col">{{ __('Valor Especial') }}</th>
+                        <th scope="col">{{ __('Data') }}</th>
                         <th scope="col">Ações</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($lista as $item)
                         <tr data-table-row-id={{ $item->id }}>
-                            <td>
-                                <button data-show-id={{ $item->id }} class="btn btn-icon btn-sm btn-primary" type="button">
-                                    <span class="btn-inner--icon"><i class="ni ni-single-copy-04"></i></span>
-                                </button>
-                            </td>
-                            <td>{{ $item->description->placa }}</td>
-                            <td>{{ $item->description->chassi }}</td>
-                            <td>{{ $item->lotacao }}</td>
-                            <td  align="center">
-                                @if ($item->arCondicionado)
-                                    <span class="badge badge-success">Possui</span>
-                                @else
-                                    <span class="badge badge-warning">Não possui</span>
-                                @endif
-                            </td>
-
-                            <td  align="center">
-                                @if ($item->acessibilidade)
-                                    <span class="badge badge-success">Possui</span>
-                                @else
-                                    <span class="badge badge-warning">Não possui</span>
-                                @endif
-                            </td>
-
-                            <td align="center">
-                                @if ($item->description->disponivel)
-                                    <span data-badge-available-id="{{ $item->id }}" class="badge badge-success">Disponível</span>
-                                @else
-                                    <span data-badge-available-id="{{ $item->id }}" class="badge badge-warning">Em manutenção</span>
-                                @endif
-                            </td>
+                            <td>{{ $item->cidade_id }}</td>
+                            <td>{{ $item->licitacao }}</td>
+                            <td>{{ $item->valorEspecial }}</td>
+                            <td>{{ $item->data }}</td>
 
                             <td align="center">
                                 <div class="dropdown">
