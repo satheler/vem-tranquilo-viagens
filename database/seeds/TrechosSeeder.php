8723c93a94f56eff0e1592adaf202e561f580558
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2019-04-21 18:48:24 -0300

    Seed - Changed: trecho attributes

--- database/seeds/TrechosSeeder.php
+++ database/seeds/TrechosSeeder.php
@@ -12,16 +12,16 @@ class TrechosSeeder extends Seeder
     public function run()
     {
         DB::table('trecho')->insert([
-            'horarioSaida' => '12:00:00',
-            'horarioChegada' => '13:00:00',
+            // 'horarioSaida' => '12:00:00',
+            // 'horarioChegada' => '13:00:00',
             'quilometragem' => 102.0,
             'origem_id' => 1,
             'destino_id' => 3
         ]);
 
         DB::table('trecho')->insert([
-            'horarioSaida' => '13:05:00',
-            'horarioChegada' => '15:30:00',
+            // 'horarioSaida' => '13:05:00',
+            // 'horarioChegada' => '15:30:00',
             'quilometragem' => 174.0,
             'origem_id' => 3,
             'destino_id' => 5
