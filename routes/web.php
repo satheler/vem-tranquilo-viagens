57a5bb2de3905b35a1058b0196c894b56d327602
Michael Martins <martins.m09@hotmail.com>
2019-04-23 18:24:06 -0300

    Frontend - Added: GerirFuncionarios

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,8 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::resource('funcionario', 'FuncionarioController');
 
+    Route::resource('gerir_funcionarios', 'GerirFuncionariosController');
+
     Route::resource('pagamento', 'FormaDePagamentoController');
 
     Route::prefix('alocacao')->name('alocacao_')->group(function () {
