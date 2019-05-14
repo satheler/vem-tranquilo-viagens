61c3ab427d8e80c2b8aa3199cf1109d5a4c5fb30
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2019-05-14 19:54:41 -0300

    feat: bug fix on inactive alocacao

--- database/seeds/AlocacaoUrbanoSeeder.php
+++ database/seeds/AlocacaoUrbanoSeeder.php
@@ -13,11 +13,16 @@ class AlocacaoUrbanoSeeder extends Seeder
     {
         DB::table('alocacao_urbano')->insert([
             'id' => 1,
-            'onibus_id' => 1,
             'trajeto_id' => 1,
-            'cobrador_id' => 1,
+            'onibus_id' => 1,
             'motorista_id' => 1,
+            'cobrador_id' => 2,
+            'auxiliar_id' => null,
+            'horarioInicio' => '05:30',
+            'horarioFim' => '09:30',
+            'data' => '2019-05-15',
         ]);
+
     }
 }
 
