596d319f920a00499cc4fb2b5f24d5ca5fe955f5
judson <judson.henrique01@gmail.com>
2019-05-22 17:17:20 -0300

    Database- Chargend: pagamento_cartao

---
+++ database/migrations/2019_05_22_170320_create_pagamento_cartao_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagamentoCartaoTable extends Migration
+{
+    private $databaseName = 'pagamento_cartao';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->databaseName, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titular');
+            $table->bigInteger('num_cartao');
+            $table->integer('cvv');
+            $table->date('data_validade');
+            $table->integer('qnt_parcelas');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       
+        Schema::dropIfExists($this->databaseName);
+    }
+}
