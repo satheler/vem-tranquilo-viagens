dbc64ca7103bb8338f40ec674be330f7bb8219a1
judson <judson.henrique01@gmail.com>
2019-05-23 17:21:35 -0300

    DataBase - VendaOnline

--- database/migrations/2019_05_22_152345_create_venda_online_table.php
+++ database/migrations/2019_05_22_152345_create_venda_online_table.php
@@ -18,6 +18,7 @@ class CreateVendaOnlineTable extends Migration
             $table->bigIncrements('id');
             $table->date('data_compra');
             $table->boolean('forma_pagamento');
+            $table->integer('num_assento');
             
             $table->unsignedBigInteger('alocacao_id');
             $table->foreign('alocacao_id')->references('id')->on('alocacao_intermunicipal');
@@ -25,18 +26,16 @@ class CreateVendaOnlineTable extends Migration
             $table->unsignedBigInteger('assento_id');
             $table->foreign('assento_id')->references('id')->on('assento');
             
-            $table->unsignedBigInteger('categoria_onibus_id');
-            $table->foreign('categoria_onibus_id')->references('id')->on('categoria_onibus');
-            
             $table->unsignedBigInteger('categoria_passageiro_id');
             $table->foreign('categoria_passageiro_id')->references('id')->on('categoria_passageiro');
             
             $table->unsignedBigInteger('tarifa_intermunicipal_id');
             $table->foreign('tarifa_intermunicipal_id')->references('id')->on('tarifa_intermunicipal');
             
+           
             
-            $table->unsignedBigInteger('users_id');
-            $table->foreign('users_id')->references('id')->on('users');
+            
+          
             
             
             
