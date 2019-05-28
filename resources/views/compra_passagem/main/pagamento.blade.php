2334cd6a490529661029d96b4fb0f735918c6e0d
Michael Martins <martins.m09@hotmail.com>
2019-05-28 15:30:02 -0300

    Change: input styles

--- resources/views/compra_passagem/main/pagamento.blade.php
+++ resources/views/compra_passagem/main/pagamento.blade.php
@@ -1,28 +1,91 @@
 @extends('compra_passagem.index', ['title' => __('Compra de Passagens')])
 
 @section('infos')
-    <div class="card-header border-0">
+    <div class="card-header bg-default border-0">
         <div class="row align-cidades-center">
             <div class="col-lg-8">
-                <h3 class="mb-0">{{ __('Informações da compra') }}</h3>
+                <h3 class="mb-0 text-white">{{ __('Alegrete') }} <i class="fas fa-arrow-right"> {{ __('Porto Alegre') }} </i> </h3>
             </div>
         </div>
+    </div>
+    <div class="card-body bg-white border-0">
+        <ul class="list-group list-group-flush" data-toggle="checklist">
+            <li class="checklist-entry list-group-item flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between">
+                    <div class="checklist-info">
+                        <div class="col">
+                            <h3 class="checklist-title">Viagem</h3>
+                            <small class="ml-3">Alegrete <i class="fas fa-chevron-right"></i> Porto Alegre</small>
+                        </div>
+                    </div>
+                    <div class="checklist-info">
+                        <div class="col">
+                            <h3 class="checklist-title">Saída e Chegada</h3>
+                            <small class="ml-3"><i class="fas fa-clock"></i> 23:30 <i class="fas fa-chevron-right"></i> 08:30</small>
+                        </div>
+                    </div>
+                </div>
+            </li> 
+            <li class="checklist-entry list-group-item flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between">
+                    <div class="checklist-info">
+                        <div class="col">
+                            <h3 class="checklist-title mb-0">PASSAGEM</h3>
+                            <small class="ml-3"><i class="fas fa-money-bill-alt"></i>  R$ 233,50</small>
+                        </div>
+                    </div>
+                    <div class="checklist-info">
+                        <div class="col">
+                            <h3 class="checklist-title mb-0">POLTRONA</h3>
+                            <div class="row justify-content-center">
+                                <button type="button" class="btn btn-primary btn-sm mb-3">06</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="checklist-info">
+                        <div class="col">
+                            <span class="badge badge-primary mt-4">EXECUTIVO</span>
+                        </div>
+                    </div>
+                </div>
+            </li> 
+            <li class="checklist-entry list-group-item flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between">
+                    <button type="button" class="btn btn-warning btn-sm">Alterar</button>
+                    <h1 class="checklist-title mb-0">Total R$ 233,50</h1>                    
+                </div>
+            </li> 
+        </ul>
     </div>
 @endsection
 
 @section('infos-pag')
-    <div class="card-header border-0">
+    <div class="card-header bg-default border-0">
         <div class="row align-cidades-center">
             <div class="col-lg-8">
-                <h3 class="mb-0">{{ __('Informe os dados para pagamento') }}</h3>
+                <h3 class="mb-0 text-white ">{{ __('Informe os dados para pagamento') }}</h3>
             </div>
         </div>
     </div>
-    <div class="card-body border-0">
+    <div class="card-body bg-white border-0">
+        <div class="row clearfix">
+            <div class="col">
+                <div class="form-group{{ $errors->has('nome') ? ' has-danger' : '' }}">
+                    <label class="form-control-label" for="input-nome"> {{ __('TITULAR DO CARTÃO') }}</label>
+                    <input type="text" name="nome" id="input-nome" class="form-control form-control-alternative{{ $errors->has('nome') ? ' is-invalid' : '' }}" placeholder="{{ __('Titular do Cartão') }}" value="{{ old('nome') }}" required autofocus>
+
+                    @if ($errors->has('nome'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('nome') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+        </div>
         <div class="row clearfix">
             <div class="col">
                 <div class="form-group{{ $errors->has('nro') ? ' has-danger' : '' }}">
-                    <label class="form-control-label text-white" for="input-nro"> {{ __('NÚMERO DO CARTÃO') }}</label>
+                    <label class="form-control-label" for="input-nro"> {{ __('NÚMERO DO CARTÃO') }}</label>
                     <input type="text" name="nro" id="input-nro" class="form-control form-control-alternative{{ $errors->has('nro') ? ' is-invalid' : '' }}" placeholder="{{ __('Número do Cartão') }}" value="{{ old('nro') }}" required autofocus>
 
                     @if ($errors->has('nro'))
@@ -36,7 +99,7 @@
         <div class="row clearfix">
             <div class="col-lg-6">
                 <div class="form-group{{ $errors->has('mes') ? ' has-danger' : '' }}">
-                    <label class="form-control-label text-white" for="input-mes">{{ __('MÊS DE VENCIMENTO') }}</label>
+                    <label class="form-control-label" for="input-mes">{{ __('MÊS DE VENCIMENTO') }}</label>
                     <input type="text" name="mes" id="input-mes" class="form-control form-control-alternative{{ $errors->has('mes') ? ' is-invalid' : '' }}" placeholder="{{ __('Mês de vencimento') }}" value="{{ old('mes') }}" required autofocus>
 
                     @if ($errors->has('mes'))
@@ -47,7 +110,7 @@
                 </div>
             </div>
             <div class="col-lg-6">
-                    <label class="form-control-label text-white" for="form-control-label"> {{__('ANO DE VENCIMENTO')}} </label>
+                    <label class="form-control-label" for="form-control-label"> {{__('ANO DE VENCIMENTO')}} </label>
                     <select bootstrapSelect name="ano"  data-size="4" data-live-search="true" required>
                         <option value="" disabled selected>Selecione ano de vencimento</option>
                             <option value="2019">2019</option>
@@ -63,7 +126,7 @@
         </div>
         <div class="row clearfix">
             <div class="col-lg-6">
-                <label class="form-control-label text-white" for="form-control-label"> {{__('PARCELAS')}} </label>
+                <label class="form-control-label  " for="form-control-label"> {{__('PARCELAS')}} </label>
                 <select bootstrapSelect name="tipo"  data-size="4" data-live-search="true" required>
                     <option value="" disabled selected>À vista</option>
                         <option value="1">1x</option>
@@ -82,7 +145,7 @@
             </div>
             <div class="col-lg-6">
                 <div class="form-group{{ $errors->has('cod') ? ' has-danger' : '' }}">
-                    <label class="form-control-label text-white" for="input-cod">{{ __('COD. SEGURANÇA') }}</label>
+                    <label class="form-control-label  " for="input-cod">{{ __('COD. SEGURANÇA') }}</label>
                     <input type="text" name="cod" id="input-cod" class="form-control form-control-alternative{{ $errors->has('cod') ? ' is-invalid' : '' }}" placeholder="{{ __('Cod. Segurança') }}" value="{{ old('cod') }}" required autofocus>
 
                     @if ($errors->has('cod'))
@@ -95,7 +158,7 @@
         </div>
         <div class="row clearfix">
             <div class="col-lg-8">
-                <h6 class="text-white">Ao clicar em Comprar, você confirma que leu e concordou com o nosso <a href="#">Termo de Aceite</a>.</h4>
+                <h6 class=" ">Ao clicar em Comprar, você confirma que leu e concordou com o nosso <a href="#">Termo de Aceite</a>.</h4>
             </div>
         </div>
         <div class="row justify-content-center">
