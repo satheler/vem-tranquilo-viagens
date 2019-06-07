faba01efccd6d3535ec72ae6db0540b14b744b4a
Sabrina <sabrinacarlew@gmail.com>
2019-06-07 00:13:44 -0300

    Model - Added: Compra

---
+++ app/Compra.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Validator;
+use App\VendaOnline;
+use App\Cliente;
+
+class Compra extends Model
+{
+    protected $table = 'compras';
+
+    public function venda()
+    {
+        return $this->belongsToMany('App\VendaOnline', 'compras', 'cliente_id', 'venda_id');
+    }
+
+    public function cliente()
+    {
+        return $this->belongsToMany('App\Cliente', 'compras', 'venda_id', 'cliente_id');
+    }
+
+    public function getAll()
+    {
+        return $this->all();
+    }
+
+    public function get(int $id)
+    {
+        return $this->find($id);
+    }
+
+    public function add(array $input)
+    {
+        $validator = Validator::make($input, [
+            'cliente_id' => 'exists:clientes,id'
+        ]);
+
+        $venda = new VendaOnline();
+        $venda->add($input);
+
+        $this->cliente_id = $input['cliente_id'];
+
+        $this->venda_id = $venda->id;
+
+        $this->save();
+
+    }
+
+}
