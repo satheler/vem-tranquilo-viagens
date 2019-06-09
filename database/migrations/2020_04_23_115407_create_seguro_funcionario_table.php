5d1c25721a7d48ccceb608a4d1de2075885cc9a7
Sabrina <sabrinacarlew@gmail.com>
2019-06-09 20:43:50 -0300

    Database - Changed: SeguroFuncionario

--- database/migrations/2020_04_23_115407_create_seguro_funcionario_table.php
+++ database/migrations/2020_04_23_115407_create_seguro_funcionario_table.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSeguroTable extends Migration
+class CreateSeguroFuncionarioTable extends Migration
 {
     private $databaseName = 'seguro_funcionario';
     /**
