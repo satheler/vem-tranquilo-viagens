7c38e88426747fef4bf202b39c8c9a70e7a05593
judson <judson.henrique01@gmail.com>
2019-06-10 00:20:59 -0300

    Model- RegistroSinistro (atualizada)

--- app/RegistroSinistro.php
+++ app/RegistroSinistro.php
@@ -33,13 +33,17 @@ class RegistroSinistro extends Model
     {
         $date = new DateTime();
         $validator = Validator::make($input, [
-            'tipo_causa'=>'regex:[a-zA-Z\u00C0-\u00FF ]',
-            'descricao_causa'=>'required|string',
-            'envolvidos'=>'required|string',
-            'custo' => 'required|numeric',
-            'descricao_custo'=> 'required|string',
+            'tipo_causa' =>'required|string', 
+            'descricao_causa'=>'required|string',        
+            'envolvidos'=> 'required|string', 
+            'custo' => 'required|numeric|min:0.01',
+           
+            'descricao_custo'=>'required|string', 
+            'responsavel_custo'=>'required|string', 
             'data' => 'required|date_format:d/m/Y|before_or_equal:'. $date->format('d/m/Y'),
-            'onibus_id'
+           
+
+         
         ]);
 
         if ($validator->fails()) {
@@ -52,6 +56,7 @@ class RegistroSinistro extends Model
         $this->envolvidos = $input['envolvidos'];
         $this->custo = $input['custo'];
         $this->descricao_custo = $input['descricao_custo'];
+        $this->responsavel_custo = $input['responsavel_custo'];
         $this->onibus_id = $input['onibus'];
 
         $dataConverter = date_create_from_format('d/m/Y', $input['data']);
@@ -60,6 +65,48 @@ class RegistroSinistro extends Model
 
         $this->save();
     }
+
+    public function edit(array $input, int $id)
+    {
+
+        $validator = $this->rules($input);
+        if ($validator->fails()) {
+            return $validator;
+        }
+
+        $sinistro = $this->find($id);
+
+        $sinistro->tipo_causa= $input['tipo_causa'];
+        $sinistro->descricao_causa = $input['descricao_causa'];
+        $sinistro->envolvidos = $input['envolvidos'];
+        $sinistro->custo = $input['custo'];
+        $sinistro->descricao_custo = $input['descricao_custo'];
+        $sinistro->descricao_custo = $input['responsavel_custo'];
+        $sinistro->onibus_id = $input['onibus'];
+
+        $dataConverter = date_create_from_format('d/m/Y', $input['data']);
+        $sinistro->data = $dataConverter->format('Y-m-d');
+
+        $sinistro->update();
+    }
+
+    private function rules(array $input){
+        $dataa = new DateTime();
+        return Validator::make($input, [
+            
+            'tipo_causa' =>'required|string', 
+            'descricao_causa'=>'required|string',        
+            'envolvidos'=> 'required|string', 
+            'custo' => 'required|numeric|min:0.01',
+            'descricao_custo'=>'required|string', 
+            'responsavel_custo'=>'required|string', 
+
+            
+            'data' => 'required|date_format:d/m/Y|before_or_equal:'. $dataa->format('d/m/Y'),
+
+        ]);
+    }
+
 
     public function remove(int $id)
     {
