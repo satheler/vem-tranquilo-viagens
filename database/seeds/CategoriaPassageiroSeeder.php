1a2f1e0f9c9d159e699eaf912be1b69f42c40dc7
judson <judson.henrique01@gmail.com>
2019-06-13 17:02:14 -0300

    Seed - CategoriaPassageiro

--- database/seeds/CategoriaPassageiroSeeder.php
+++ database/seeds/CategoriaPassageiroSeeder.php
@@ -10,13 +10,31 @@ class CategoriaPassageiroSeeder extends Seeder
      * @return void
      */
     public function run()
+    
     {
         DB::table('categoria_passageiro')->insert([
             'id' => 1,
+            'tipo' => 'Normal',
+            'desconto' => 00,
+          
+        ]);
+        DB::table('categoria_passageiro')->insert([
+            'id' => 2,
             'tipo' => 'estudante',
             'desconto' => 10,
           
         ]);
-    
+        DB::table('categoria_passageiro')->insert([
+            'id' => 3,
+            'tipo' => 'idoso',
+            'desconto' => 50,
+          
+        ]);
+        DB::table('categoria_passageiro')->insert([
+            'id' => 4,
+            'tipo' => 'especial',
+            'desconto' => 100,
+          
+        ]);
     }
 }
