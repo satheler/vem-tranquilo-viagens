b775bf9357479993bd2b02f563053c7f63e2ca7f
judson <judson.henrique01@gmail.com>
2019-06-13 17:12:02 -0300

    Seed - TarifaIntermunicipal

--- database/seeds/TarifaIntermunicipalSeeder.php
+++ database/seeds/TarifaIntermunicipalSeeder.php
@@ -16,5 +16,41 @@ class TarifaIntermunicipalSeeder extends Seeder
             'id' => 1,
             'valor' => 123,
         ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 2,
+            'valor' => 203,
+        ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 3,
+            'valor' => 100,
+        ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 4,
+            'valor' => 200,
+        ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 5,
+            'valor' => 250,
+        ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 6,
+            'valor' => 105,
+        ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 7,
+            'valor' => 174,
+        ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 8,
+            'valor' => 50,
+        ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 9,
+            'valor' => 00,
+        ]);
+        DB::table('tarifa_intermunicipal')->insert([
+            'id' => 10,
+            'valor' => 180,
+        ]);
     }
 }
