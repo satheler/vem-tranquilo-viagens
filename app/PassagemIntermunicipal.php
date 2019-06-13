04102860ec7dc9b0c11a4b1c851d674134f70fde
Sabrina <sabrinacarlew@gmail.com>
2019-06-13 16:21:43 -0300

    Model - Changed: Assento

--- app/PassagemIntermunicipal.php
+++ app/PassagemIntermunicipal.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Assento;
 
 class PassagemIntermunicipal extends Model
 {
@@ -20,7 +21,13 @@ class PassagemIntermunicipal extends Model
         
         $this->valor = $input['valor'];
         $this->users_id = Auth::user()->id;
-        $this->alocacao_id = $input['alocacao_intermunicipal_id'];       
+        $this->alocacao_id = $input['alocacao_intermunicipal_id'];
+        
+        foreach ($input['poltronas'] as $poltrona) {
+            $assento = new Assento();
+            $assento->add(['num_assento' => $poltrona, 'alocacao_id' => $input['alocacao_intermunicipal_id']]);
+        }
+
         
         $this->save();
             
