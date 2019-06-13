8488fbe1bda6efad52371bed0e99e79724a46ac6
judson <judson.henrique01@gmail.com>
2019-06-13 03:34:45 -0300

    Controller - Listar

--- app/Http/Controllers/ContadorLucroController.php
+++ app/Http/Controllers/ContadorLucroController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Exception;
+use App\CompraPassagem;
 
 class ContadorLucroController extends Controller
 {
@@ -14,8 +15,41 @@ class ContadorLucroController extends Controller
      */
     public function index()
     {
+        // $contador = new Contador();
+        // $lista = $contador->getAll();
         return view('contador_lucro.main.index');
     }
+
+
+
+    // public function search(Request $request) {
+    //     $origem = $request->input('origem');
+    //     $destino = $request->input('destino');
+
+    //     $dataConverter = date_create_from_format('d/m/Y', $request->input('data_ida'));
+    //     $data_ida = $dataConverter->format('Y-m-d');
+
+    //     if($request->input('data_volta') !== null) {
+    //         $dataConverter = date_create_from_format('d/m/Y', $request->input('data_volta'));
+    //         $data_volta = $dataConverter->format('Y-m-d');
+    //     }
+
+    //     return redirect()
+    //         ->route('page_compra.list', ["origem" => $origem, "destino" => $destino, "data_ida" => $data_ida, "data_volta" => $data_volta ?? null]);
+    // }
+
+    public function list($categoria_passageiro, $alocacao_intermunicipal, $tarifa_intermunicipal)
+    {
+       
+
+        $compras = new CompraPassagem();
+        $comp = $compras->getByFilter($categoria_passageiro_id, $alocacao_intermunicipal_id, $tarifa_intermunicipal_id);
+        return view('compra.main.index', compact('categoria_passageiro_id', 'alocacao_intermunicipal_id', 'tarida_intermunicipal_id'));
+    }
+
+
+       
+
 
     /**
      * Show the form for creating a new resource.
@@ -55,10 +89,7 @@ class ContadorLucroController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        return view('contador_lucro.main.edit');
-    }
+
 
     /**
      * Update the specified resource in storage.
