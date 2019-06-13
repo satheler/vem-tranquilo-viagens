16aaa0f953306ebc0900b04697d9f8924773a4eb
Sabrina <sabrinacarlew@gmail.com>
2019-06-13 02:46:51 -0300

    View - Changed: List Trajetos

--- resources/views/compra/main/list.blade.php
+++ resources/views/compra/main/list.blade.php
@@ -12,33 +12,37 @@
                 <div class="card-body">
                     <ul class="list-group list-group-flush list my--3">
                         @foreach ($trajetos as $trajeto)
+    
                             <li class="list-group-item px-0" style="background-color: transparent;">
                                 <form method="POST" action="{{ route('page_compra.poltrona') }}">
-                                    <input type="hidden" name="alocacao_id" value="{{ $trajeto['info']->id }}">
-                                    <input type="hidden" name="trecho_origem_id" value="{{ $trajeto['info']->origem->id }}">
-                                    <input type="hidden" name="trecho_destino_id" value="{{ $trajeto['info']->destino->id }}">
-                                    <input type="hidden" name="valor" value="{{ $trajeto["valor"] }}">
-                                    <div class="row align-items-center text-center">
-                                        <div class="col">
-                                            <h3 class="mb-0" data-toggle="tooltip" data-placement="top" title="Horário da partida">{{ $trajeto['info']->origem->horarioSaida }}</h3>
-                                            <h3 class="mb-0" data-toggle="tooltip" data-placement="bottom" title="Hora da Chegada. Esta informação é apenas para efeito de orientação e não deve ser utilizada para compromissos agendados, pois, trata-se de apenas uma previsão de chegada ao destino.">{{ $trajeto['info']->destino->horarioSaida }}</h3>
-                                        </div>
-                                        <div class="col">
-                                            <h5 class="mb-0" data-toggle="tooltip" data-placement="top" title="Tipo de ônibus">{{ $trajeto['onibus']->categoria->categoria }}</h5>
-                                            <h5 class="mb-0">{0} assentos livres</h5>
-                                        </div>
-                                        <div class="col">
-                                            <div class="row">
-                                                <div class="col pr-1 text-right"><small class="mb-0" style="vertical-align: bottom;">R$ </small></div>
-                                                <div class="col p-0 text-left"><h3 class="mb-0">  {{ $trajeto["valor"] }}</h3></div>
+                                    @foreach ($trajeto['info'] as $encontrado)
+                                       <input type="hidden" name="alocacao_id" value="{{ $encontrado->id }}">
+                                        <input type="hidden" name="trecho_origem_id" value="{{ $encontrado->origem->id }}">
+                                        <input type="hidden" name="trecho_destino_id" value="{{ $encontrado->destino->id }}">
+                                        <input type="hidden" name="valor" value="{{ $trajeto["valor"] }}">
+                                        <div class="row align-items-center text-center">
+                                            <div class="col">
+                                                <h3 class="mb-0" data-toggle="tooltip" data-placement="top" title="Horário da partida">{{ $encontrado->origem->horarioSaida }}</h3>
+                                                <h3 class="mb-0" data-toggle="tooltip" data-placement="bottom" title="Hora da Chegada. Esta informação é apenas para efeito de orientação e não deve ser utilizada para compromissos agendados, pois, trata-se de apenas uma previsão de chegada ao destino.">{{ $encontrado->destino->horarioSaida }}</h3>
+                                            </div>
+                                            <div class="col">
+                                                <h5 class="mb-0" data-toggle="tooltip" data-placement="top" title="Tipo de ônibus">{{ $trajeto['onibus']->categoria->categoria }}</h5>
+                                                <h5 class="mb-0" data-toggle="tooltip" data-placement="top" title="Assentos Disponíveis">{{ $trajeto["assentos"] }}</h5>
+                                            </div>
+                                            <div class="col">
+                                                <div class="row">
+                                                    <div class="col pr-1 text-right"><small class="mb-0" style="vertical-align: bottom;">R$ </small></div>
+                                                    <div class="col p-0 text-left"><h3 class="mb-0">  {{ $trajeto["valor"] }}</h3></div>
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <button type="submit" class="btn btn-primary" >{{ __('SELECIONAR') }}</button>
                                             </div>
                                         </div>
-                                        <div class="col-2">
-                                            <button type="submit" class="btn btn-primary" >{{ __('SELECIONAR') }}</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </li>
+                                    </form>
+                                </li>
+                            
+                            @endforeach
                         @endforeach
                     </ul>
                 </div>
