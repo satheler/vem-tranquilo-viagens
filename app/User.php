cc6bd5ca4dc54e225e27b06fc838841e05156cce
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2019-07-04 15:59:32 -0300

    fix: cadastro usuário

--- app/User.php
+++ app/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'passwgitord',
+        'name', 'email', 'password',
     ];
 
     /**
